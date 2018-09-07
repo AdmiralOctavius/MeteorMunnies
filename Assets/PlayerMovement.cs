2d1c6f3f04de50c0d24b35275fa0a52f3bd49081
AdmiralOctavius <buildgamesnotbombs@gmail.com>
2018-09-07 09:53:07 -0400

    Initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed = 3.5f;
+	// Use this for initialization
+	void Start () {
+        Application.targetFrameRate = 60;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //To get frame independant gameplay, factor in DeltaTime. Delta time checks how much time has passed since the last frame
+        if (Input.GetKey(KeyCode.W)){
+            gameObject.transform.position += new Vector3(0, speed) * Time.deltaTime;
+        }
+	}
+}
